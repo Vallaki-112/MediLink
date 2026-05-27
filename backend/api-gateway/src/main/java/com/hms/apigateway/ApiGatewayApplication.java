32f3514b86467c1746ceb1d8c54ed14aa6db83fe
Vallaki-112 <vallaki.11feb@gmail.com>
2026-05-27 16:43:28 +0530

    Infrastructure: Successfully configured Parent BOM, Eureka Discovery Server, and API Gateway on Java 21

---
+++ backend/api-gateway/src/main/java/com/hms/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.hms.apigateway;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // Tells Eureka that this gateway is part of the microservice ecosystem
+
+public class ApiGatewayApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
